30e59069d37e70e243d07c7f1b7825ac67da4b6c
shubham1994s <shubhamkawle41@gamil.com>
2022-11-14 11:43:46 +0530

    comit  by shubham

--- SwachhBharatAPI/Models/IdentityModels.cs
+++ SwachhBharatAPI/Models/IdentityModels.cs
@@ -18,9 +18,12 @@ namespace SwachhBharatAPI.Models
             return userIdentity;
         }
 
-        internal Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager userManager)
+        internal Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            throw new NotImplementedException();
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity =  manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
         }
     }
 
